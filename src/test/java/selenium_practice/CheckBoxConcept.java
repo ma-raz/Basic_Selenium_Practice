9518fc2cd6ba5031a3e55b70b31453bedaff5bc9
Kartick <kartick01@yahoo.com>
2018-07-20 04:49:26 -0400

    New Class CheckBox added

---
+++ src/test/java/selenium_practice/CheckBoxConcept.java
@@ -0,0 +1,49 @@
+package selenium_practice;
+
+import io.github.bonigarcia.wdm.ChromeDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+import java.util.concurrent.TimeUnit;
+
+public class CheckBoxConcept {
+    protected WebDriver driver;
+
+    @Before
+    public void setUp(){
+        ChromeDriverManager.getInstance().setup();
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.google.com");
+
+        driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().timeouts().setScriptTimeout(10,TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(2,TimeUnit.SECONDS);
+
+        driver.manage().deleteAllCookies();
+        driver.manage().window().maximize();
+
+    }
+    @Test
+    public void checkBox() throws InterruptedException {
+        WebElement element = driver.findElement(By.xpath("//a[@id = 'warehouse-locations-dropdown']"));
+        Actions action = new Actions(driver);
+        action.moveToElement(element).build().perform();
+        Thread.sleep(2000);
+        WebElement element1 = driver.findElement(By.xpath("//div[@class = 'style-check']/label[@title ='Gas Station' and @for = 'hasGas-desktop']"));
+        element1.click();
+    }
+
+    @After
+    public void tearDown(){
+        driver.close();
+        driver.quit();
+    }
+
+
+}
